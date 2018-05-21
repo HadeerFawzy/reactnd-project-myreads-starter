fd61ad98caa598cd5bbd588876aaf772d6ffff7b
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-05-21 15:27:16 +0200

    filter books according to shelves

--- src/MyReads.js
+++ src/MyReads.js
@@ -17,27 +17,29 @@ class MyReads extends Component {
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   { books.map((book) => ( 
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, background: `url(${book.imageLinks.thumbnail})`}}>
+                    book.shelf == 'currentlyReading' && (
+                      <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, background: `url(${book.imageLinks.thumbnail})`}}>
+                            </div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
                           </div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">
+                            {book.authors}
+                          </div> 
                         </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">
-                          {book.authors}
-                        </div> 
-                      </div>
-                    </li>
+                      </li>
+                    )
                   ))}
                 </ol>
               </div>
@@ -46,7 +48,31 @@ class MyReads extends Component {
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  
+                  { books.map((book) => ( 
+                    book.shelf == 'wantToRead' && (
+                      <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, background: `url(${book.imageLinks.thumbnail})`}}>
+                            </div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">
+                            {book.authors}
+                          </div> 
+                        </div>
+                      </li>
+                    )
+                  ))}
                 </ol>
               </div>
             </div>
@@ -54,7 +80,31 @@ class MyReads extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  
+                  { books.map((book) => ( 
+                    book.shelf == 'read' && (
+                      <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, background: `url(${book.imageLinks.thumbnail})`}}>
+                            </div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">
+                            {book.authors}
+                          </div> 
+                        </div>
+                      </li>
+                    )
+                  ))}
                 </ol>
               </div>
             </div>
