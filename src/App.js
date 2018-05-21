5eae301e90b93d63a1f425117c676ea6f02f6971
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-05-21 13:46:34 +0200

    make ajax request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Search from './Search.js'
 import MyReads from './MyReads.js'
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
-
+    books: []
   }
-
+  componentDidMount (){
+    BooksAPI.getAll().then((books) => {
+      console.log(books);
+      this.setState({books: books})
+    })
+  }
   render() {
     return (
       <div className="app">
